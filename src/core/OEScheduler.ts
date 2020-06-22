02d66ab8adb7cc63c69c2ed80b522805cb8c77da
dcmunhoz <dc.munhoz@hotmail.com>
2020-06-22 13:25:50 -0300

    Task execution

--- src/core/OEScheduler.ts
+++ src/core/OEScheduler.ts
@@ -1,6 +1,12 @@
+require('dotenv/config');
+const util = require('util');
+const exec = util.promisify(require("child_process").exec);
+const fs = require("fs");
+const path = require('path')
+
 interface taskParamsInterface {
     executed: boolean,
-    nextExecution: Date | null
+    nextExecution: Date
 }
 
 interface Task {
@@ -13,9 +19,12 @@ type EachPeriodType = "Day" | "Week" | "Month" | "Year";
 export default class OEScheduler {
 
     private tickTime: number = 1; //Segundos para verificação
+    private scriptsPath: string = "";
 
     public constructor()
     {
+
+        this.scriptsPath = (process.env.SCRIPTS_LOCATION) ? process.env.SCRIPTS_LOCATION : path.resolve(__dirname, '..', '..', 'scripts');
 
     }
 
@@ -28,43 +37,73 @@ export default class OEScheduler {
      */
     public assign(eachPeriod: EachPeriodType, task: Task): void
     {
+        if (!process.env.PROGRESS_EXE || !process.env.PF_FILE) {
+            console.error("Verifique se as variaveis de embiente PROGRESS_EXE e PF_FILE estão preenchidas")
+            return;
+        }
+
         const startDate = this.parseDate(task.startDate);
         const taskParams: taskParamsInterface = {
             executed: false,
-            nextExecution: startDate
+            nextExecution: new Date(startDate)
         }
         
         console.log(`${task.run} Cadastrado\n Próxima execução => ${taskParams.nextExecution?.toLocaleDateString()} ${taskParams.nextExecution?.toLocaleTimeString()}`);
 
-        setInterval(() => {
-            
+        setInterval(async () => {
+               
             const today = new Date();
             
-            const atTime = taskParams.nextExecution?.getHours() + ":" + taskParams.nextExecution?.getMinutes();
+            const atTime = taskParams.nextExecution.getHours() + ":" + taskParams.nextExecution.getMinutes();
             const todayTime = today.getHours() + ":" + today.getMinutes();
 
             if (atTime === todayTime && !taskParams.executed) {
                 
                 if (taskParams.nextExecution?.toLocaleDateString() == today.toLocaleDateString()) {
-                    console.log("Executa a tarefa aqui");
+                    console.log(`*** INICIANDO EXECUÇÃO DE SCRIPT ${task.run} ***`)
+                    console.log("-- Validando caminho")
+                    if (!fs.existsSync(this.scriptsPath + '\\' + task.run)) {
+                        console.error("    Script "+ task.run +" não localizado no caminho: " + this.scriptsPath);
+                        console.error("    Tarefa abortada, verifique se o script existe e esta localizado na pasta correta.");
+                        taskParams.nextExecution.setMinutes(taskParams.nextExecution.getMinutes() + 1);
+                        console.info("    Tentativa de proxima execução marcada para o minuto seguinte: ", taskParams.nextExecution);
+                        return;
+                    }
+                    console.log("-- OK");
+                    console.log("*** INICIANDO EXECUÇÃO ***");
                     taskParams.executed = true;
 
-                    switch(eachPeriod){
-                        case "Day":
-                            taskParams.nextExecution.setMinutes(taskParams.nextExecution.getMinutes() + 1);
-                        break;
-                        case "Week":
-                            taskParams.nextExecution.setDate(taskParams.nextExecution.getDate() + 7);
-                        break;
-                        case "Month":
-                            taskParams.nextExecution.setMonth(taskParams.nextExecution.getMonth() + 1);
-                        break;
-                        case "Year":
-                            taskParams.nextExecution.setFullYear(taskParams.nextExecution.getFullYear() + 1);
-                        break;
+                    try { 
+
+                        const { stdin, stderr } = await exec(`${process.env.PROGRESS_EXE} -b -pf ${process.env.PF_FILE} -p ${this.scriptsPath + '\\' + task.run}`);
+
+                        switch(eachPeriod){
+                            case "Day":
+                                taskParams.nextExecution.setDate(taskParams.nextExecution.getDate() + 1);
+                            break;
+                            case "Week":
+                                taskParams.nextExecution.setDate(taskParams.nextExecution.getDate() + 7);
+                            break;
+                            case "Month":
+                                taskParams.nextExecution.setMonth(taskParams.nextExecution.getMonth() + 1);
+                            break;
+                            case "Year":
+                                taskParams.nextExecution.setFullYear(taskParams.nextExecution.getFullYear() + 1);
+                            break;
+                        }
+                        
+                        taskParams.nextExecution.setMinutes(startDate.getMinutes());
+                        console.log(`    ${task.run} executada com sucesso, próxima execução => ${taskParams.nextExecution?.toLocaleDateString()} ${taskParams.nextExecution?.toLocaleTimeString()}`);    
+
+                    }catch(err){
+                        if (err) {
+                            console.error("-- Houve um erro ao executar o script, tafera abortada");
+                            console.error("    ", err)
+                            return
+                        }
                     }
 
-                    console.log(`${task.run} executada com sucesso, próxima execução => ${taskParams.nextExecution?.toLocaleDateString()} ${taskParams.nextExecution?.toLocaleTimeString()}`);
+                    
                 }
 
             }else if (atTime !== todayTime && taskParams.executed){
@@ -75,7 +114,7 @@ export default class OEScheduler {
 
         }, this.tickTime * 1000);
     }
-
+    
     /**
      * 
      * Converte a data para o formato da classe Date( ) e retorna o objeto.
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import OEScheduler from './core/OEScheduler';
 const scheduler = new OEScheduler();
 
 scheduler.assign("Day", { 
-    run: "ExportaBenef.p",
-    startDate: "15/06/2020 18:49"
-});
-
-scheduler.assign("Day", {
-    run: "OutroScript.P",
-    startDate: "15/06/2020 20:40"
+    run: "expbenef.p",
+    startDate: "22/06/2020 13:16"
 });
